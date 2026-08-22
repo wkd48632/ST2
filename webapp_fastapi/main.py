6531b36c60117af12ae34e09dd0e43f7e0b071f3
wkd48632 <wkd48632@gmail.com>
2026-08-22 16:34:30 +0900

    fastapi

---
+++ webapp_fastapi/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return FileResponse("index.html")
+
+@app.get('/python_to_browser')
+def python_to_browser():
+    return 'hello from python'
+
+@app.post("/browser_to_python")
+def get_data(data:dict):
+    print(data)
